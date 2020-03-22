6fd067cedc950a504935badef708a1ff519c3e8e
Michał Wróblewski <michal.wroblewski@polkomtel.com.pl>
2020-03-22 18:22:02 +0100

    cw2_good

---
+++ cw2/cw2/Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Dupka sortowanie={orderBy}";
+        }
+
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        //[HttpPut]
+       
+        //[HttpDelete]
+
+
+    }
+  
+   
+}
---
+++ cw2/cw2/Models/Update.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace cw2.Models
